baf82df53c692041ed503e683b6759c3dfe3a51b
Dilshod Fayzullayev <darkprohub-uz@yandex.com>
2022-08-04 22:19:45 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/restfullspringcompanyapp/RestfullSpringCompanyAppApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.restfullspringcompanyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullSpringCompanyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfullSpringCompanyAppApplication.class, args);
+    }
+
+}
